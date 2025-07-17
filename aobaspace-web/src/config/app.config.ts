c431dcd1622b045e3763517207c263ce33371b1d
Jacob Chirayil <jacob.chirayil@grapeseed.com>
2025-07-16 23:59:59 -0400

    web .env workaround (hardcoded)

--- aobaspace-web/src/config/app.config.ts
+++ aobaspace-web/src/config/app.config.ts
@@ -2,10 +2,10 @@
 // whether the code is running on the server (during SSR/RSC) or on the client.
 
 // This is the URL used by the browser. It must be accessible from the user's machine.
-const API_URL_CLIENT = process.env.NEXT_PUBLIC_API_URL;
+const API_URL_CLIENT = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
 
 // This is the URL used by the Next.js server. It must use the internal Docker network hostname.
-const API_URL_SERVER = process.env.API_URL_SERVER;
+const API_URL_SERVER = process.env.API_URL_SERVER || 'http://aobaspace-api:3000/api';
 
 // Check if the code is running on the server or the client.
 const isServer = typeof window === 'undefined';
